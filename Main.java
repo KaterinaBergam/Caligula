2e72c139a6ff43bd2b8baf67de244fb0d4fb0300
KaterinaBergam <157735407+KaterinaBergam@users.noreply.github.com>
2024-02-03 22:18:14 +0300

    new test

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("caligula"); // test
         System.out.println("hello"); // test 2
+        System.out.println("new caligula"); // new test
     }
 }
