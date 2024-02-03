ee2771a6777f8735f6344bb0a6c3c2d3ab7df677
Denis <temp4m@yandex.ru>
2024-02-03 22:20:57 +0300

    test calig2

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("caligula"); // test
         System.out.println("hello"); // test 2
+<<<<<<< Updated upstream
+=======
+        System.out.println("new caligula"); // new test
+        System.out.println("new caligula 3"); // new test 2
+>>>>>>> Stashed changes
     }
 }
