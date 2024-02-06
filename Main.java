03efafff8fa850b8056637bbf3c0955f8f7975a6
Denis <temp4m@yandex.ru>
2024-02-06 23:01:04 +0300

    обновил классы
    перенес метод конвертации числа в класс Number
    для отладки добавил вывод полей  класса number
    
    нужно написать код перевода числа из арабского

--- Main.java
+++ Main.java
@@ -1,87 +1,16 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Number num1 = new Number();
-        Number num2 = new Number();
-
-        System.out.println(num2.type);
-
         System.out.println("caligula"); // test
         System.out.println("hello"); // test 2
         //System.out.println("new caligula"); // new test
         //System.out.println("new caligula 3"); // new test 2
 
+        Number num1 = new Number();
 
-        double firstNum = 0.0;
-
-        System.out.println("Hello World\n");
-        Scanner scanNum = new Scanner(System.in);
-        if (scanNum.hasNextInt()) {
-            double numRome = scanNum.nextDouble();
-            System.out.println(numRome);
-            System.out.println("double");
-        }
-        else {
-            String numRome = scanNum.nextLine();
-            System.out.println(numRome);
-            System.out.println("string");
-            System.out.println(romeTransform(numRome));
-            System.out.println("romeTransformed");
-
-            scanNum.close();
-        }
-    }
-
-    public static double romeTransform(String romeNumber) {
-        double romeNumToArabicResult = 0.0;
-        int romeStringLenght = romeNumber.length();
-        romeNumber = romeNumber.toUpperCase();
-        double[] arabicNumberMass = new double[romeStringLenght];
-        for(int i = 0; i < romeStringLenght; i++) {
-            switch (romeNumber.charAt(i)) {
-                case 'I':
-                    //romeNumToArabicResult += 1;
-                    arabicNumberMass[i] = 1;
-                    break;
-                case 'V':
-                    //romeNumToArabicResult += 5;
-                    arabicNumberMass[i] = 5;
-                    break;
-                case 'X':
-                    //romeNumToArabicResult += 10;
-                    arabicNumberMass[i] = 10;
-                    break;
-                case 'L':
-                    //romeNumToArabicResult += 50;
-                    arabicNumberMass[i] = 50;
-                    break;
-                case 'C':
-                    //romeNumToArabicResult += 100;
-                    arabicNumberMass[i] = 100;
-                    break;
-                case 'D':
-                    //romeNumToArabicResult += 500;
-                    arabicNumberMass[i] = 500;
-                    break;
-                case 'M':
-                    //romeNumToArabicResult += 1000;
-                    arabicNumberMass[i] = 1000;
-                    break;
-                default:
-                    break;
-            }
-        }
-        
-        romeNumToArabicResult += arabicNumberMass[0];
-        for (int i = 1; i < arabicNumberMass.length; i++) {
-            if (arabicNumberMass[i-1] < arabicNumberMass[i]) {
-                //arabicNumberMass[i] *= -1;
-                romeNumToArabicResult += arabicNumberMass[i] - arabicNumberMass[i-1] * 2;
-            }
-            else {
-                romeNumToArabicResult += arabicNumberMass[i];
-            }
-        }
-        return romeNumToArabicResult;
+        System.out.println("\ninput number chars:");
+        System.out.println("type: " + num1.type);
+        System.out.println("romanian view: " + num1.valueRomanian);
+        System.out.println("arabic view: " + num1.valueArabic);
     }
 }
